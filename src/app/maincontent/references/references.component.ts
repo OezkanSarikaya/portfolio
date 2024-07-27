fd3ef339bd2fe891dc66984d1b2d681c2da76ad5
Özkan Sarikaya <oezkan.sarikaya@freenet.de>
2024-07-27 16:41:27 +0200

    update testimonials section, content slider

--- src/app/maincontent/references/references.component.ts
+++ src/app/maincontent/references/references.component.ts
@@ -12,10 +12,58 @@ export class ReferencesComponent {
     text: string;
     image: string;
     person: string;
-}[] = [
-    { text: 'Özkan has delivered outstanding performance in our team project. He possesses a deep understanding of JavaScript and has effectively utilized this knowledge to solve complex problems and develop innovative solutions. His ability to write clear and efficient code has significantly advanced our project. Özkan also excels in teamwork, contributing valuable ideas and actively supporting other team members. His expertise and dedication have been instrumental to the success of our project.', image: './assets/img/Mask group.png', person: 'Alain Soltau - Team Partner' },
-    { text: 'I was fortunate to work with Özkan. He is a true Team-Player, which with his high level of javascript made teamwork easy and fun. He was always available for any questions or doubts no matter the day of the week or time. He is a person I would always work with if given the choice.', image: '', person: 'Raudel Gómez Smith - Team Partner' },
-    { text: '', image: '', person: 'Peter' },
-  
+  }[] = [
+    {
+      text: 'Özkan has delivered outstanding performance in our team project. He possesses a deep understanding of JavaScript and has effectively utilized this knowledge to solve complex problems and develop innovative solutions. His ability to write clear and efficient code has significantly advanced our project. Özkan also excels in teamwork, contributing valuable ideas and actively supporting other team members. His expertise and dedication have been instrumental to the success of our project.',
+      image: './assets/img/personB.png',
+      person: 'Alain Soltau - Team Partner',
+    },
+    {
+      text: 'I was fortunate to work with Özkan. He is a true Team-Player, which with his high level of javascript made teamwork easy and fun. He was always available for any questions or doubts no matter the day of the week or time. He is a person I would always work with if given the choice.',
+      image: './assets/img/raudel.png',
+      person: 'Raudel Gómez Smith - Team Partner',
+    },
+    {
+      text: 'Durch seine kreativen Ideen und Programmierkenntnisse, hatte das Unternehmen einen Wettbewerbsvorteil und ein Alleinstellungsmerkmal.',
+      image: './assets/img/Mask group.png',
+      person: 'Peter',
+    },
   ];
+
+  currentText: string = this.testimonials[0].text;
+  currentPerson: string = this.testimonials[0].person;
+  currentImage: string = this.testimonials[0].image;
+  currentPostId: number = 0;
+  currentAnimate:string = "";
+
+  navigate(postId: number) {
+    this.currentText = this.testimonials[postId].text;
+    this.currentPerson = this.testimonials[postId].person;
+    this.currentImage = this.testimonials[postId].image;
+    this.currentPostId = postId;
+  }
+
+  next() {
+    this.currentAnimate = "";
+    if (this.currentPostId >= this.testimonials.length-1) {this.currentPostId=-1}
+    let postId = this.currentPostId+1;
+    this.currentText = this.testimonials[postId].text;
+    this.currentPerson = this.testimonials[postId].person;
+    this.currentImage = this.testimonials[postId].image;
+    this.currentPostId = postId;
+    this.currentAnimate = "animate__fadeIn";
+  }
+
+  back() {
+    this.currentAnimate = "";
+    if (this.currentPostId == 0) {
+      this.currentPostId = this.testimonials.length;
+    }
+    let postId = this.currentPostId - 1;
+    this.currentText = this.testimonials[postId].text;
+    this.currentPerson = this.testimonials[postId].person;
+    this.currentImage = this.testimonials[postId].image;
+    this.currentPostId = postId;
+    this.currentAnimate = "animate__fadeIn";
+  }
 }

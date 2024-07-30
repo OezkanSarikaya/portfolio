fe3f58837bf54f7ab82260d7920eff73863faff9
Özkan Sarikaya <oezkan.sarikaya@freenet.de>
2024-07-30 11:25:55 +0200

    responsive updates all sections

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -6,12 +6,43 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [CommonModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
+
+
+
 export class HeaderComponent {
   mobileNavOpen = false;
+
+  mobileMenu = document.getElementById('mobileMenu');
+
   toggleMobileNav() {
-          this.mobileNavOpen =  !this.mobileNavOpen;
+    this.mobileNavOpen = !this.mobileNavOpen;
+    const body = document.getElementsByTagName('body')[0];
+
+    // let mobileMenu = document.getElementById('mobileMenu');
+    if (this.mobileNavOpen) {
+      body.classList.add('noscroll');
+      if (this.mobileMenu) {
+        this.mobileMenu.classList.add('animate__slideInRight');
+      }
+      
+      // this.mobileMenu.classList.add('animate__slideInRight');
+    } else {
+      body.classList.remove('noscroll');
+      if (this.mobileMenu) {
+        this.mobileMenu.classList.add('animate__slideOutRight');
+      }
+      // mobileNav.classList.add('animate__slideOutRight');
+    }
   }
 
+  // constructor() {
+  //   const body = document.getElementsByTagName('body')[0];
+  //   body.classList.add('noscroll');
+  // }
+  // ngOnDestroy() {
+  //    const body = document.getElementsByTagName('body')[0];
+  //    body.classList.remove('noscroll');
+  // }
 }

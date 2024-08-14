9be8bd743fed7ad9958b3a6894ad407cd620121e
Özkan Sarikaya <oezkan.sarikaya@freenet.de>
2024-08-14 14:32:11 +0200

    responsive text in hero section and htaccess setup

--- src/app/maincontent/hero/hero.component.ts
+++ src/app/maincontent/hero/hero.component.ts
@@ -2,11 +2,12 @@ import { Component, inject } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 import { TranslationService } from '../../translation.service';
 import { EmailComponent } from '../../email/email.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-hero',
   standalone: true,
-  imports: [TranslateModule,EmailComponent],
+  imports: [TranslateModule,EmailComponent,CommonModule],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.scss'
 })

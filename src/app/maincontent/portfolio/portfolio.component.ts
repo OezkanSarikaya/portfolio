33425da8f009aef22e4dca3c37084a37cd6cbcec
Özkan Sarikaya <oezkan.sarikaya@freenet.de>
2025-03-06 10:46:10 +0100

    Update Portfolio Data

--- src/app/maincontent/portfolio/portfolio.component.ts
+++ src/app/maincontent/portfolio/portfolio.component.ts
@@ -16,15 +16,15 @@ export class PortfolioComponent {
 
   portfolios: Portfolio[] = [
     {
-      headline: 'Join',
-      subheadline: 'JavaScript | HTML | CSS | Firebase',
-      text: 'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories.',
-      deutsch:'Aufgabenmanager nach dem Vorbild des Kanban-Systems. Erstellen und organisieren Sie Aufgaben mit Hilfe von Drag-and-Drop-Funktionen, weisen Sie Benutzer und Kategorien zu.',
-      image:'./../../../assets/img/projectimages/join.png',
-      githubUrl:'https://github.com/OezkanSarikaya/join',
-      demoUrl:'https://join.oezkan-sarikaya.de/',
+      headline: 'Videoflix',
+      subheadline: 'Angular | Python | Django REST Framework',
+      text: 'A Video Streaming Platform just like Netflix',
+      deutsch:'Eine Video-Streaming-Plattform wie Netflix',
+      image:'./../../../assets/img/projectimages/videoflix.png',
+      githubUrl:'https://github.com/OezkanSarikaya/videoflix_backend',
+      demoUrl:'https://videoflix.oezkan-sarikaya.de/',
       index: 1
-    },
+    }, 
     {
       headline: 'Coderr',
       subheadline: 'JavaScript | Python | Django REST Framework',
@@ -35,6 +35,17 @@ export class PortfolioComponent {
       demoUrl:'https://coderr.oezkan-sarikaya.de/',
       index: 2
     }, 
+    {
+      headline: 'Join',
+      subheadline: 'JavaScript | Python | Django REST Framework',
+      text: 'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories.',
+      deutsch:'Aufgabenmanager nach dem Vorbild des Kanban-Systems. Erstellen und organisieren Sie Aufgaben mit Hilfe von Drag-and-Drop-Funktionen, weisen Sie Benutzer und Kategorien zu.',
+      image:'./../../../assets/img/projectimages/join.png',
+      githubUrl:'https://github.com/OezkanSarikaya/join',
+      demoUrl:'https://join.oezkan-sarikaya.de/',
+      index: 3
+    },
+
 
     // {
     //   headline: 'Pokedex',
@@ -54,8 +65,9 @@ export class PortfolioComponent {
       image:'./../../../assets/img/projectimages/visualdenker.png',
       githubUrl:'',
       demoUrl:'https://www.visualdenker.com/',
-      index: 3
+      index: 4
     }, 
+
     {
       headline: 'El Pollo Loco',
       subheadline: 'JavaScript | HTML | CSS',
@@ -64,7 +76,7 @@ export class PortfolioComponent {
       image:'./../../../assets/img/projectimages/el-pollo-loco.png',
       githubUrl:'https://github.com/OezkanSarikaya/canvasGame',
       demoUrl:'https://el-pollo-loco.oezkan-sarikaya.de/',
-      index: 4
+      index: 5
     },
 
   ];
